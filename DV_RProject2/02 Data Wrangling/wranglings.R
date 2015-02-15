2c275fc3fbaa6e61efaa40824c4c2a671bfb4bc8
Travis S. Cox <traviscox1990@utexas.edu>
2015-02-14 20:13:51 -0600

    made another wrangling and visualization for it

--- DV_RProject2/02 Data Wrangling/wranglings.R
+++ DV_RProject2/02 Data Wrangling/wranglings.R
@@ -1,4 +1,8 @@
+library("plyr")
 library("dplyr")
 library("tidyr")
 G1 <- geo_springs %>% select(DATALABEL,LOCATION) %>% distinct() %>% filter(!is.na(LOCATION)) %>% slice(1:18)
 tbl_df(G1)
+
+G2 <- geo_springs %>% group_by(LOCATION) %>% summarise(NUMBER_DATAPOINTS=n())
+tbl_df(G2)
--- DV_RProject2/03 Visualizations/visualizations.R
+++ DV_RProject2/03 Visualizations/visualizations.R
@@ -1,3 +1,7 @@
+library("gplots")
+library("ggplot2")
+library("reshape2")
 
 ggplot(G1, aes(x = DATALABEL,y = LOCATION)) + geom_point(colour="red")
 
+ggplot(G2, aes(x = LOCATION)) + geom_histogram(fill="darkorange",colour="white",aes(weight=NUMBER_DATAPOINTS)) + coord_flip()
