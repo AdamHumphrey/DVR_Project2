e9c3c0af9eb8f4fff6dc2ce07be108342f70ab2f
Adam Humphrey <adamthumph@gmail.com>
2015-02-16 17:57:06 -0600

    Title change round 2

--- DV_RProject2/03 Visualizations/visualizations.R
+++ DV_RProject2/03 Visualizations/visualizations.R
@@ -6,5 +6,4 @@ ggplot(G1, aes(x = DATALABEL,y = LOCATION)) + geom_point(colour="red") + ggtitle
 
 ggplot(G2, aes(x = LOCATION)) + geom_histogram(fill="darkorange",colour="white",aes(weight=NUMBER_DATAPOINTS)) + ggtitle ("Location vs. Number of datapoints") + ylab("Number of Datapoints") + coord_flip()
 
-# This graph shows the average for each drain period that was measured.
-ggplot(G3, aes(x = PERIOD, y = AVERAGE)) + geom_area(colour="red") + ggtitle(Average Drainage per Observation Period)
+ggplot(G3, aes(x = PERIOD, y = AVERAGE)) + geom_area(colour="red") + ggtitle("Average Drainage for Cold Springs per Observation Period")
