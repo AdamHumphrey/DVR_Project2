43bf6f73ef6a2811a654fabc6bae0c8bf7da663d
Travis S. Cox <traviscox1990@utexas.edu>
2015-02-15 23:33:15 -0600

    added titles to the graphs

--- DV_RProject2/03 Visualizations/visualizations.R
+++ DV_RProject2/03 Visualizations/visualizations.R
@@ -2,9 +2,9 @@ library("gplots")
 library("ggplot2")
 library("reshape2")
 
-ggplot(G1, aes(x = DATALABEL,y = LOCATION)) + geom_point(colour="red")
+ggplot(G1, aes(x = DATALABEL,y = LOCATION)) + geom_point(colour="red") + ggtitle("Location vs. Datalabel")
 
-ggplot(G2, aes(x = LOCATION)) + geom_histogram(fill="darkorange",colour="white",aes(weight=NUMBER_DATAPOINTS)) + coord_flip()
+ggplot(G2, aes(x = LOCATION)) + geom_histogram(fill="darkorange",colour="white",aes(weight=NUMBER_DATAPOINTS)) + ggtitle ("Location vs. Number of datapoints") + ylab("Number of Datapoints") + coord_flip()
 
 # This graph shows the average for each drain period that was measured.
-ggplot(G3, aes(x = PERIOD, y = AVERAGE)) + geom_area(colour="red")
+ggplot(G3, aes(x = PERIOD, y = AVERAGE)) + geom_area(colour="red") + ggtitle("Average vs. Period")
